6f93063513413741a5d7e0413f5eb4045417e663
eddsanity <eyadhossamm@gmail.com>
2019-10-29 17:07:42 +0200

    Finished this sprint's version of UserHandler

--- OnlineStore/UserHandler.cs
+++ OnlineStore/UserHandler.cs
@@ -5,27 +5,32 @@ namespace OnlineStore
     class UserHandler
     {
         SqlConnection dbConnection;
+        AuthenticationHandler authHandler;
         public UserHandler(string serverName)
         {
             dbConnection = new SqlConnection("Data Source=" + serverName + ";Initial Catalog=emailApp;Integrated Security=True");
+            authHandler = new AuthenticationHandler(serverName);
             dbConnection.Open();
         }
 
         public void CreateAccount(User usr)
         {
-            // TODO: verification that username and e-mail aren't taken
-            UserInfo uInfo = usr.GetUserInfo();
-            string query = "insert into [USER](USERNAME, EMAIL, PASSWORD) ";
-            string arg = "values('" + uInfo.GetUsername() + "', '" + uInfo.GetEmail() + "', '" + uInfo.GetPassword() + "');";
-
-            SqlCommand cmd = new SqlCommand(query + arg, dbConnection);
-            cmd.ExecuteNonQuery();
+            string username = usr.GetUserInfo().GetUsername();
+            string email = usr.GetUserInfo().GetEmail();
+            string pwd = usr.GetUserInfo().GetPassword();
+            int type = (int)usr.GetUserInfo().GetType();
+            if(!authHandler.VerifyEmail(email) && !authHandler.VerifyUsername(username))
+            {
+                string query = "insert into [USER](USERNAME, EMAIL, PASSWORD, TYPE) ";
+                string arg = "values('" + username + "', '" + email + "', '" + pwd + "', '" + type + "');";
+                SqlCommand cmd = new SqlCommand(query + arg, dbConnection);
+                cmd.ExecuteNonQuery();
+            }
         }
 
-        public bool Login(string usrORemail, string encryptedPwd)
+        public bool Login(User usr)
         {
-            // TODO: verify credentials before logging in, needs AuthenticationHandler
-            if (true)
+            if (authHandler.VerifyUser(usr))
                 return true;
             else return false;
         }
