1527eb9fd9079d4abb24a4824befab55fc9f45dc
eddsanity <eyadhossamm@gmail.com>
2019-10-29 13:22:59 +0200

    Added ConsumerUser class

---
+++ OnlineStore/ConsumerUser.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineStore
+{
+    class ConsumerUser : IUser
+    {
+        public string u_username { set; get; }
+        public string u_email    { set; get; }
+        public string u_password { set; get; }
+    }
+}
