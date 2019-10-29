9ea5d59f4aee14f04aeb3c3fbc4cb743fa3fb5eb
eddsanity <eyadhossamm@gmail.com>
2019-10-29 13:30:05 +0200

    Added AdminUser class

---
+++ OnlineStore/AdminUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore
+{
+    class AdminUser : IUser
+    {
+        public string u_username { set; get; }
+        public string u_email    { set; get; }
+        public string u_password { set; get; }
+    }
+}
--- OnlineStore/Product.cs
+++ OnlineStore/Product.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace OnlineStore
 {
     class Product
     {
         private string p_name;
-        private float p_price;
+        private float  p_price;
         private string p_category;
     }
 }
