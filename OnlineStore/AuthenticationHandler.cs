7b2be9bc3a147b5548f973a7287d8fc720e7a6f7
eddsanity <eyadhossamm@gmail.com>
2019-10-29 16:59:59 +0200

    Finished AuthenticationHandler

---
+++ OnlineStore/AuthenticationHandler.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace OnlineStore
+{
+    class AuthenticationHandler
+    {
+        SqlConnection dbConnection;
+
+        public AuthenticationHandler(string serverName)
+        {
+            dbConnection = new SqlConnection("Data Source=" + serverName + ";Initial Catalog=emailApp;Integrated Security=True");
+            dbConnection.Open();
+        }
+
+        public bool VerifyUser(User user)
+        {
+            string username = user.GetUserInfo().GetUsername();
+            string email    = user.GetUserInfo().GetEmail();
+            string pwd      = user.GetUserInfo().GetPassword();
+            if ((VerifyUsername(username) || VerifyEmail(email)))
+                if (VerifyPassword((VerifyEmail(email) ? email : username), pwd))
+                    return true;
+            return false;
+        }
+
+        public bool VerifyUsername(string username)
+        {
+            string query = "SELECT count(USERID) from [USER] where USERNAME = '" + username + "';";
+            SqlCommand cmd = new SqlCommand(query, dbConnection);
+            int usernameCheck = Convert.ToInt16(cmd.ExecuteScalar());
+            if (usernameCheck == 1)
+                return true;
+            return false;
+        }
+
+        public bool VerifyEmail(string email)
+        {
+            string query = "SELECT count(USERID) from [USER] where EMAIL = '" + email + "';";
+            SqlCommand cmd = new SqlCommand(query, dbConnection);
+            int emailCheck = Convert.ToInt16(cmd.ExecuteScalar());
+            if (emailCheck == 1)
+                return true;
+            return false;
+        }
+
+        public bool VerifyPassword(string usrORemail, string encryptedPwd)
+        {
+            string query = "";
+            if(VerifyEmail(usrORemail))
+                query = "SELECT count(USERID) from [USER] where EMAIL = '" + usrORemail + "' AND PASSWORD = '" + encryptedPwd + "';";
+            if(VerifyUsername(usrORemail))
+                query = "SELECT count(USERID) from [USER] where USERNAME = '" + usrORemail + "' AND PASSWORD = '" + encryptedPwd + "';";
+
+            SqlCommand cmd = new SqlCommand(query, dbConnection);
+            int pwdCheck = Convert.ToInt16(cmd.ExecuteScalar());
+            if (pwdCheck == 1)
+                return true;
+            return false;
+        }
+
+        public void CloseConnection()
+        {
+            dbConnection.Close();
+        }
+    }
+}
