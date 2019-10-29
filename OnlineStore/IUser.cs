262e8b1735bfabe65b0d1eedabf2f6d0ea288501
eddsanity <eyadhossamm@gmail.com>
2019-10-29 13:07:46 +0200

    created user interface

---
+++ OnlineStore/IUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore
+{
+    interface IUser
+    {
+        string u_username { get; set; }
+        string u_email    { get; set; }
+        string u_password { get; set; }
+    }
+}
