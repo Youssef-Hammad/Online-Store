ac2bc525c5fa45e0b4751b3b9ee0a45a93c53b9d
youssef88999 <youssef88999@outlook.com>
2019-10-29 20:18:23 +0200

    Added AdminHandler

---
+++ OnlineStore/AdminHandler.cs
@@ -0,0 +1,28 @@
+namespace OnlineStore
+{
+    class AdminHandler
+    {
+        private SqlConnection dbConnection;
+
+        public AdminHandler(string serverName)
+        {
+            // edit connection string to real values
+            connectionString = "Data Source=" + serverName + ";Initial Catalog=db.sql;User ID=UserName;Password=Password";
+
+            dbConnection = new SqlConnection(connectionString);
+            dbConnection.Open();
+        }
+
+        public bool VerifyIsAdmin(User admin)
+        {
+            if (admin.GetUserInfo().GetType() == ADMIN)
+                return true;
+            else
+                return false;
+        }
+        public void closeConnection()
+        {
+            dbConnection.Close();
+        }
+    }
+}
--- OnlineStore/Store.cs
+++ OnlineStore/Store.cs
@@ -1,13 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace OnlineStore
 {
     class Store
     {
         private string s_StoreName;
+
+        public void SetStoreName(string newName)
+        {
+            s_StoreName = newName;
+        }
+
+        public string GetStoreName()
+        {
+            return s_StoreName;
+        }
     }
 }
