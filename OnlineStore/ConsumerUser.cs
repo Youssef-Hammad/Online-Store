d27b6406f1bc18c2fdd04727b64509283e661e0f
eddsanity <eyadhossamm@gmail.com>
2019-10-29 14:43:40 +0200

    Reverted the changes done

--- OnlineStore/ConsumerUser.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OnlineStore
-{
-    class ConsumerUser : IUser
-    {
-        public string u_username { set; get; }
-        public string u_email    { set; get; }
-        public string u_password { set; get; }
-    }
-}
--- OnlineStore/IUser.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OnlineStore
-{
-    interface IUser
-    {
-        string u_username { get; set; }
-        string u_email    { get; set; }
-        string u_password { get; set; }
-    }
-}
