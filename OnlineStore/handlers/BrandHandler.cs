ea490bae160baedb2c74f9a0e66a3441eab560df
youssef88999 <youssef88999@outlook.com>
2019-11-09 11:44:57 +0200

    Added BrandHandler Added Dispose to StatisticsHandler

---
+++ OnlineStore/handlers/BrandHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.handlers
+{
+    class BrandHandler
+    {
+        SqlConnection dbConnection;
+
+        public BrandHandler(string connString)
+        {
+            dbConnection = new SqlConnection(connString);
+            dbConnection.Open();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing == true)
+            {
+                dbConnection.Close();
+            }
+        }
+
+        ~BrandHandler()
+        {
+            Dispose(false);
+        }
+
+    }
+}
--- OnlineStore/handlers/StatisticsHandler.cs
+++ OnlineStore/handlers/StatisticsHandler.cs
@@ -108,5 +108,24 @@ namespace OnlineStore
             return returnList;
         }
 
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing == true)
+            {
+                dbConnection.Close();
+            }
+        }
+
+        ~StatisticsHandler()
+        {
+            Dispose(false);
+        }
+
     }
 }
