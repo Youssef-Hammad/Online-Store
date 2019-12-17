f8aa5b0dfe3b5a3c732b9a4b960be97c4b3f1780
eddsanity <eyadhossamm@gmail.com>
2019-12-17 22:53:38 +0200

    Added WebServiceHandler and modified UserHandler to use the WebServiceHandler instead of direct DB access

---
+++ OnlineStore/handlers/WebServiceHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace OnlineStore
+{
+    public class WebServiceHandler
+    {
+        protected string baseURL;
+        protected string entity;
+        protected string secString;
+        protected string param;
+
+        public WebServiceHandler(string tmpEntity = "", string tmpParam = "")
+        {
+            baseURL = "http://okboomer-api.somee.com/APIDIR/api";
+            entity = tmpEntity;
+            secString = "CWyNnomeXehvyucs";
+            param = tmpParam;
+        }
+
+        public HttpWebResponse POST()
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(baseURL + "/" + entity + "/" + secString);
+            var Data = Encoding.ASCII.GetBytes(param);
+            req.Method = "POST";
+            req.ContentType = "application/json";
+            req.ContentLength = Data.Length;
+
+            using (var stream = req.GetRequestStream())
+            {
+                stream.Write(Data, 0, Data.Length);
+            }
+
+            HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+            return res;
+        }
+
+        public HttpWebResponse GET()
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(baseURL + "/" + entity + "/" + secString + "/" + param);
+            HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+            return res;
+        }
+
+        public string GetResponseBody(HttpWebResponse responseMsg)
+        {
+            return new StreamReader(responseMsg.GetResponseStream()).ReadToEnd();
+        }
+    }
+}
